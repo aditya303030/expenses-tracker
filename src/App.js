643c66b7c0d37cc3988f8dd69756391fd4c70e7d
aditya303030 <73743156+aditya303030@users.noreply.github.com>
2022-04-08 15:42:10 +0530

    deleted old componets, new component added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React,{useState} from 'react';
 import './App.css';
-import Form from './components/form';
-import ExpenseList from './components/expenseList';
 
 function App() {
   //states
-  const [inputText,setInputText] = useState("");
-  const [expense,setExpense] = useState([]);
-  const [amount,setAmount] = useState();
-  const [currentAmount,setCurrentAmount] = useState([]);
+  // const [inputText,setInputText] = useState("");
+  // const [expense,setExpense] = useState([]);
+  // const [amount,setAmount] = useState();
+  // const [currentAmount,setCurrentAmount] = useState([]);
   // let total_amount = 0;
   // for(let i=0;i<currentAmount.length;i++) {
   //   for(const [key,value] of Object.entries(currentAmount[i])) {
@@ -26,9 +24,6 @@ function App() {
       <div className='header'>
         <h1>Expenses tracker</h1>
       </div>
-      <Form currentAmount={currentAmount} amount={amount} setCurrentAmount={setCurrentAmount} setAmount = {setAmount} expense ={expense} setExpense = {setExpense} inputText = {inputText} setInputText={setInputText} />
-      <ExpenseList setExpense={setExpense} amount = {amount} setAmount={setAmount} expense = {expense} currentAmount={currentAmount} setCurrentAmount={setCurrentAmount} />
-      
     </div>
   );
 }
